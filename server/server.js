b679dfdec195f238c03991433d4d7c8a567f02da
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-05-17 16:38:33 -0500

    added /games

--- server/server.js
+++ server/server.js
@@ -6,6 +6,29 @@ const express = require('express');
 // create the app
 const app = express();
 
+let games = [
+    {
+        name: 'Minecraft',
+        funLevel: 7
+    },
+    {
+        name: 'CoD',
+        funLevel: 8.7
+    },
+];
+
+// client wants to get the games from us
+app.get('/games', (req, res) => { /* pass in arrow function, arguments: request, response */
+    // code runs when someone asks to get games
+    // ie: localhost:5000/games
+
+    console.log('in /games page')
+    // send back the games array to client, could send back anything
+    res.send(games);
+})
+
+
+
 // listening for requests
 app.listen(5000, () => {
     console.log('network up and listening')
